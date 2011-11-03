29b497a71c507748542d211cf2b92e86bac8a273
Andriy Yanko <andriy.yanko@gmail.com>
2011-11-03 12:19:28 +0200

    Don't abort execution if git repository is not yet ready

--- lib/capistrano/uptodate.rb
+++ lib/capistrano/uptodate.rb
@@ -19,19 +19,26 @@ Capistrano::Configuration.instance.load do
     end
 
     task :git do
+      # skip if no git dir detected
       git_dir = fetch(:uptodate_local_reposytory, `#{scm_binary} rev-parse --git-dir`.strip)
-      abort "Can't detect git repository" if git_dir.empty?
+      next if git_dir.empty?
 
+      # skip if remote is not yet configured
+      remote_reference_file = File.join(git_dir, 'refs', 'remotes', remote_repository, branch)
+      next unless File.exists?(remote_reference_file)
+
+      # fetch remote references and skip on error
       fetch_file = File.join(git_dir, "FETCH_HEAD")
-
       unless File.exist?(fetch_file) && Time.now - File.mtime(fetch_file) < time
-        Capistrano::CLI.ui.say "Fetching references from #{repository} repository ..."
-        system("#{scm_binary} fetch #{remote_repository}")
+        Capistrano::CLI.ui.say "Fetching references from #{remote_repository} repository ..."
+        next unless system("#{scm_binary} fetch #{remote_repository}")
       end
 
+      # get commit for local and remote reference
       local_commit  = `#{scm_binary} rev-parse #{branch}`.strip
       remote_commit = `#{scm_binary} rev-parse #{remote_repository}/#{branch}`.strip
 
+      # compare local and remote commit
       if local_commit != remote_commit
         Capistrano::CLI.ui.say "Local '#{branch}' branch is not synchronized with '#{remote_repository}' repository."
 
